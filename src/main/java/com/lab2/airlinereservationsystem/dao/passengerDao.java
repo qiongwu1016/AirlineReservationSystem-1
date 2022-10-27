0330c653bf5a71ea97b0a842a9cb299eb3eab856
Yikang Chen <>
2022-10-27 10:58:47 -0700

    update datasource

--- src/main/java/com/lab2/airlinereservationsystem/dao/passengerDao.java
+++ src/main/java/com/lab2/airlinereservationsystem/dao/passengerDao.java
@@ -1,6 +1,15 @@
 package com.lab2.airlinereservationsystem.dao;
 
+import com.lab2.airlinereservationsystem.entity.Passenger;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
-public class passengerDao {
+@Component
+@Transactional
+public interface passengerDao extends JpaRepository<Passenger, String> {
+
+    // get a passenger
+
 
 }
