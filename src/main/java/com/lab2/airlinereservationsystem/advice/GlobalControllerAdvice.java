44dacf02f0c29eca190ca9b80af917199e36d65b
Yikang Chen <>
2022-11-01 21:06:04 -0700

    update respone for xml

--- src/main/java/com/lab2/airlinereservationsystem/advice/GlobalControllerAdvice.java
+++ src/main/java/com/lab2/airlinereservationsystem/advice/GlobalControllerAdvice.java
@@ -4,21 +4,28 @@ import com.lab2.airlinereservationsystem.common.domain.BadResponse;
 import com.lab2.airlinereservationsystem.common.domain.Response;
 import com.lab2.airlinereservationsystem.common.exception.ErrorExceptionWrapper;
 import com.lab2.airlinereservationsystem.common.exception.ValidExceptionWrapper;
+import com.lab2.airlinereservationsystem.utils.ResponseUtil;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.servlet.http.HttpServletRequest;
 
 
 @RestControllerAdvice
 public class GlobalControllerAdvice {
 
     @ExceptionHandler(value = {ErrorExceptionWrapper.class,Exception.class})
-    public BadResponse handlerError(Exception e){
-        return BadResponse.fail(400,e.getMessage());
+    public ResponseEntity<?> handlerError(Exception e, HttpServletRequest request){
+        boolean xml = Boolean.parseBoolean(request.getParameter("xml"));
+        return ResponseUtil.convertResponseEntity(BadResponse.fail(400,e.getMessage()),xml);
+
     }
 
     @ExceptionHandler(value = {ValidExceptionWrapper.class})
-    public BadResponse handlerValidError(Exception e){
-        return BadResponse.fail(404,e.getMessage());
+    public ResponseEntity<?> handlerValidError(Exception e, HttpServletRequest request){
+        boolean xml = Boolean.parseBoolean(request.getParameter("xml"));
+        return ResponseUtil.convertResponseEntity(BadResponse.fail(404,e.getMessage()),xml);
     }
 
 }
--- src/main/java/com/lab2/airlinereservationsystem/common/domain/BadResponse.java
+++ src/main/java/com/lab2/airlinereservationsystem/common/domain/BadResponse.java
@@ -6,10 +6,13 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.xml.bind.annotation.XmlRootElement;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@XmlRootElement
 public class BadResponse {
     @JsonProperty("BadRequest")
     private Response BadRequest;
