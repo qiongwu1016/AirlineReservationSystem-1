d318698c601f7a540701a4649a6f60cccc307cff
Yikang Chen <>
2022-11-08 09:30:33 -0800

    added flight xml ability

--- src/main/java/com/lab2/airlinereservationsystem/entity/Flight.java
+++ src/main/java/com/lab2/airlinereservationsystem/entity/Flight.java
@@ -6,11 +6,13 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.xml.bind.annotation.XmlRootElement;
 import java.util.*;
 
 @Entity
 @Getter
 @Setter
+@XmlRootElement
 @IdClass(FlightKey.class)
 public class Flight {
     // part of the primary key
